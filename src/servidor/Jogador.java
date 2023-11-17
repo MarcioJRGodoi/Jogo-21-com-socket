d99ba20d665d1cf04aac53e3b337140e19ec842e
Marcio.Godoi <marciojrgodoi@gmail.com>
2023-11-17 17:33:48 -0300

    correcao da regra do Às

--- src/servidor/Jogador.java
+++ src/servidor/Jogador.java
@@ -9,95 +9,83 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Jogador {
-	private Socket socket;
+    private Socket socket;
     private BufferedReader in;
     private PrintWriter out;
     private int pontuacao;
     private List<String> cartas;
     private List<Carta> asNaManga;
 
-    public Jogador(Socket socket) throws IOException 
-    {
-    	this.socket = socket;
+    public Jogador(Socket socket) throws IOException {
+        this.socket = socket;
         this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         this.out = new PrintWriter(socket.getOutputStream(), true);
         this.pontuacao = 0;
         this.cartas = new ArrayList<>();
         this.asNaManga = new ArrayList<>();
     }
-    
+
     public void mostrarMensagem() throws IOException {
-    	out.println("Quer pegar uma carta?");
+        out.println("Quer pegar uma carta?");
     }
 
-    public boolean querCarta(Baralho baralho) throws IOException 
-    {   	
-         if(cartas.isEmpty() ||Integer.parseInt(in.readLine()) == 1) 
-         {
-        	 Carta carta = baralho.pegarCarta();
-         	if(carta.getNome().contains("Ás")) 
-         	{
-         		asNaManga.add(carta);
-         	}
-         	if(asNaManga.size() > 0 && pontuacao > 10) {
-         		pontuacao -= 10;
-         		asNaManga.remove(0);
-         	}
-         	pontuacao += carta.getValor();
-                 cartas.add(carta.getNome());
-        	 return true;
-         }
-         return false;
+    public boolean querCarta(Baralho baralho) throws IOException {
+        if(cartas.isEmpty() || Integer.parseInt(in.readLine()) == 1) {
+            Carta carta = baralho.pegarCarta();
+            if(carta.getNome().contains("Ás")) {
+                asNaManga.add(carta);
+            }
+            pontuacao += carta.getValor();
+            cartas.add(carta.getNome());
+                if(asNaManga.size() > 0 && (pontuacao - asNaManga.get(0).getValor()) > 10) {
+                    pontuacao -= 10;
+                    asNaManga.remove(0);
+            }
+            return true;
+        }
+        return false;
     }
 
     public int getPontuacao() {
         return pontuacao;
     }
-    
+
     public String primeiraCarta() {
-    	return cartas.get(0);
+        return cartas.get(0);
     }
-    
-    public void mostrarCartasDaMesa(int numCartasAdversario, String cartaadv) 
-    {
-    	out.println("--------------------------------------------------------");
+
+    public void mostrarCartasDaMesa(int numCartasAdversario, String cartaadv) {
+        out.println("--------------------------------------------------------");
         out.println("Suas Cartas: " + cartas);
         out.println("Número de cartas do adversário: " + numCartasAdversario);
         out.println("Carta aberta adversario: " + cartaadv);
         out.println("--------------------------------------------------------");
-        //out.println();
-        //out.println("*");
     }
-    
+
     public int getNumCartas() {
         return cartas.size();
     }
-    
-    public void resultado(int pontuacaoAdversario) 
-    {
-    	if (pontuacao > 21) {
-    	    if (pontuacaoAdversario > 21) {
-    	        out.println("Empate!");
-    	    } else {
-    	        out.println("Você perdeu!");
-    	    }
-    	} else if (pontuacaoAdversario > 21 || pontuacao > pontuacaoAdversario) {
-    	    out.println("Você ganhou!");
-    	} else if (pontuacao == pontuacaoAdversario) {
-    	    out.println("Empate!");
-    	} else {
-    	    out.println("Você perdeu!");
-    	}
-    	out.println("Sua pontuacao: " +getPontuacao() + " Pontuacao do Adversario: " +pontuacaoAdversario);
-    	//out.println("*");
-    }
-    
-    public void close() throws IOException 
-    {
-		in.close();
-		out.close();
-		socket.close();
 
+    public void resultado(int pontuacaoAdversario) {
+        if (pontuacao > 21) {
+            if (pontuacaoAdversario > 21) {
+                out.println("Empate!");
+            } else {
+                out.println("Você perdeu!");
+            }
+        } else if (pontuacaoAdversario > 21 || pontuacao > pontuacaoAdversario) {
+            out.println("Você ganhou!");
+        } else if (pontuacao == pontuacaoAdversario) {
+            out.println("Empate!");
+        } else {
+            out.println("Você perdeu!");
+        }
+        out.println("Sua pontuacao: " + getPontuacao() + " Pontuacao do Adversario: " + pontuacaoAdversario);
     }
 
+    public void close() throws IOException {
+        in.close();
+        out.close();
+        socket.close();
+    }
 }
